e4e0dd6b2f2b6b3b8527cadda17fbf2b66c8bda3
SinLemon <shirokaras@qq.com>
2017-11-06 17:22:21 +0800

    Initial Commit

---
+++ ShirokarasNotebook/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ShirokarasNotebook
+//
+//  Created by SinLemon on 2017/11/6.
+//  Copyright © 2017年 SinLemon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ ShirokarasNotebook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShirokarasNotebook
+//
+//  Created by SinLemon on 2017/11/6.
+//  Copyright © 2017年 SinLemon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
