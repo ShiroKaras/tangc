e06d8725e4de1a438daebd7e9015fad1285877d6
SinLemon <shirokaras@qq.com>
2017-11-08 15:36:54 +0800

    Update

---
+++ ShirokarasNotebook/UI/Homepage/SKHomepageViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKHomepageViewController.h
+//  ShirokarasNotebook
+//
+//  Created by SinLemon on 2017/11/7.
+//  Copyright © 2017年 SinLemon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SKHomepageViewController : ViewController
+
+@end
---
+++ ShirokarasNotebook/UI/Homepage/SKHomepageViewController.m
@@ -0,0 +1,28 @@
+//
+//  SKHomepageViewController.m
+//  ShirokarasNotebook
+//
+//  Created by SinLemon on 2017/11/7.
+//  Copyright © 2017年 SinLemon. All rights reserved.
+//
+
+#import "SKHomepageViewController.h"
+
+@interface SKHomepageViewController ()
+
+@end
+
+@implementation SKHomepageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.automaticallyAdjustsScrollViewInsets = false;
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+@end
