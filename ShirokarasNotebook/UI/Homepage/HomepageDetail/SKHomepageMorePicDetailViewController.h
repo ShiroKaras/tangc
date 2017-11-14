98ae396f65afb75831f42625225f990234e3adfc
SinLemon <shirokaras@qq.com>
2017-11-14 16:59:00 +0800

    Add - Homepage more pic detail view controller.

---
+++ ShirokarasNotebook/UI/Homepage/HomepageDetail/SKHomepageMorePicDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKHomepageMorePicDetailViewController.h
+//  ShirokarasNotebook
+//
+//  Created by SinLemon on 2017/11/14.
+//  Copyright © 2017年 SinLemon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKHomepageMorePicDetailViewController : UIViewController
+
+@end
