a72cb83eb3bbd2871b3cc2f74425180ac3b531e6
SinLemon <shirokaras@qq.com>
2017-11-15 15:24:25 +0800

    Update - Homepage

--- ShirokarasNotebook/UI/Homepage/SKHomepageViewController.m
+++ ShirokarasNotebook/UI/Homepage/SKHomepageViewController.m
@@ -14,6 +14,11 @@
 #define TITLEVIEW_WIDTH ROUND_WIDTH_FLOAT(240)
 #define TITLEVIEW_HEIGHT ROUND_HEIGHT_FLOAT(44)
 
+typedef NS_ENUM(NSInteger, SKHomepageSelectedType) {
+    SKHomepageSelectedTypeFollow,
+    SKHomepageSelectedTypeHot
+};
+
 @interface SKHomepageViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) NSMutableArray *dataArray;
@@ -22,6 +27,7 @@
 @property (nonatomic, strong) UIButton *button_follow;
 @property (nonatomic, strong) UIButton *button_hot;
 @property (nonatomic, strong) UIView *markLine;
+@property (nonatomic, assign) SKHomepageSelectedType selectedType;
 @end
 
 @implementation SKHomepageViewController
@@ -35,9 +41,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    self.automaticallyAdjustsScrollViewInsets = false;
     self.view.backgroundColor = [UIColor whiteColor];
     [self createUI];
+    [self addObserver:self forKeyPath:@"selectedType" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:nil];
+}
+
+- (void)dealloc {
+    [self removeObserver:self forKeyPath:@"selectedType"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -82,6 +92,7 @@
     [self.tableView addSubview:_titleView];
     
     _button_follow = [UIButton new];
+    [_button_follow addTarget:self action:@selector(didClickFollowButton:) forControlEvents:UIControlEventTouchUpInside];
     [_button_follow setTitle:@"关注" forState:UIControlStateNormal];
     [_button_follow setTitleColor:COMMON_TEXT_COLOR forState:UIControlStateNormal];
     _button_follow.titleLabel.font = PINGFANG_ROUND_FONT_OF_SIZE(15);
@@ -91,6 +102,7 @@
     [_titleView addSubview:_button_follow];
     
     _button_hot = [UIButton new];
+    [_button_hot addTarget:self action:@selector(didClickHotButton:) forControlEvents:UIControlEventTouchUpInside];
     [_button_hot setTitle:@"热门" forState:UIControlStateNormal];
     [_button_hot setTitleColor:COMMON_TEXT_PLACEHOLDER_COLOR forState:UIControlStateNormal];
     _button_hot.titleLabel.font = PINGFANG_ROUND_FONT_OF_SIZE(15);
@@ -99,11 +111,20 @@
     _button_hot.right = _titleView.width-ROUND_WIDTH_FLOAT(60);
     [_titleView addSubview:_button_hot];
     
-    _markLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ROUND_WIDTH_FLOAT(20), 1)];
+    _markLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ROUND_WIDTH_FLOAT(20), 2)];
     _markLine.backgroundColor = [UIColor colorWithHex:0x37ECBA];
     _markLine.centerX = _button_follow.centerX;
     _markLine.bottom = _titleView.height;
     [_titleView addSubview:_markLine];
+    self.selectedType = SKHomepageSelectedTypeFollow;
+}
+
+- (void)didClickFollowButton:(UIButton*)sender {
+    self.selectedType = SKHomepageSelectedTypeFollow;
+}
+
+- (void)didClickHotButton:(UIButton*)sender {
+    self.selectedType = SKHomepageSelectedTypeHot;
 }
 
 #pragma mark - UITableView Delegate
@@ -153,6 +174,9 @@
             _button_follow.top = 0;
             _button_hot.centerX = _titleView.width/2 +60;
             _button_hot.top = 0;
+            
+            _markLine.bottom = _titleView.height;
+            _markLine.centerX = _button_follow.centerX;
         } completion:^(BOOL finished) {
             [_titleView removeFromSuperview];
             _titleView.frame = CGRectMake((self.view.width-_titleView.width)/2, HEADERVIEW_HEIGHT-TITLEVIEW_HEIGHT/2, TITLEVIEW_WIDTH, TITLEVIEW_HEIGHT);
@@ -168,11 +192,36 @@
             _button_follow.top = 20;
             _button_hot.centerX = _titleView.width/2 +60;
             _button_hot.top = 20;
+            
+            _markLine.bottom = _titleView.height;
+            _markLine.centerX = _button_follow.centerX;
         } completion:^(BOOL finished) {
             [_titleView removeFromSuperview];
             _titleView.frame = CGRectMake(0, 0, SCREEN_WIDTH, 20+TITLEVIEW_HEIGHT);
             [self.view addSubview:_titleView];
         }];
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"selectedType"]) {
+        if (self.selectedType==SKHomepageSelectedTypeFollow) {
+            [_button_follow setTitleColor:COMMON_TEXT_COLOR forState:UIControlStateNormal];
+            [_button_hot setTitleColor:COMMON_TEXT_PLACEHOLDER_COLOR forState:UIControlStateNormal];
+            [UIView animateWithDuration:0.2 animations:^{
+                _markLine.centerX = _button_follow.centerX;
+                _markLine.bottom = _titleView.height;
+            }];
+        } else if(self.selectedType==SKHomepageSelectedTypeHot){
+            [_button_follow setTitleColor:COMMON_TEXT_PLACEHOLDER_COLOR forState:UIControlStateNormal];
+            [_button_hot setTitleColor:COMMON_TEXT_COLOR forState:UIControlStateNormal];
+            [UIView animateWithDuration:0.2 animations:^{
+                _markLine.centerX = _button_hot.centerX;
+                _markLine.bottom = _titleView.height;
+            }];
+        } else {
+            
+        }
     }
 }
 
