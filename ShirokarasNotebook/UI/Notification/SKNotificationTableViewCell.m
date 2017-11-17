3b92582607f1d7178c8f1a8f95cc95590a96cab3
SinLemon <shirokaras@qq.com>
2017-11-17 17:37:39 +0800

    Update - Notification cell

--- ShirokarasNotebook/UI/Notification/SKNotificationTableViewCell.m
+++ ShirokarasNotebook/UI/Notification/SKNotificationTableViewCell.m
@@ -8,17 +8,66 @@
 
 #import "SKNotificationTableViewCell.h"
 
+@interface SKNotificationTableViewCell ()
+@property (nonatomic, strong) UIImageView *avatarImageView;
+@property (nonatomic, strong) UILabel *usernameLabel;
+@property (nonatomic, strong) UILabel *dateLabel;
+@property (nonatomic, strong) UILabel *contentLabel;
+@end
+
 @implementation SKNotificationTableViewCell
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    if (self == [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        self.backgroundColor = [UIColor whiteColor];
+        self.layer.masksToBounds = YES;
+        
+        _avatarImageView = [UIImageView new];
+        _avatarImageView.backgroundColor = COMMON_SEPARATOR_COLOR;
+        _avatarImageView.layer.cornerRadius = ROUND_WIDTH_FLOAT(15);
+        _avatarImageView.layer.masksToBounds = YES;
+        _avatarImageView.size = CGSizeMake(ROUND_WIDTH_FLOAT(30), ROUND_WIDTH_FLOAT(30));
+        _avatarImageView.left = ROUND_WIDTH_FLOAT(15);
+        _avatarImageView.top = ROUND_WIDTH_FLOAT(17.5);
+        [self.contentView addSubview:_avatarImageView];
+        
+        _usernameLabel = [UILabel new];
+        _usernameLabel.text = @"卓大王";
+        _usernameLabel.textColor = COMMON_TEXT_COLOR;
+        _usernameLabel.font = PINGFANG_FONT_OF_SIZE(12);
+        [_usernameLabel sizeToFit];
+        _usernameLabel.left = _avatarImageView.right+10;
+        _usernameLabel.centerY = _avatarImageView.centerY;
+        [self.contentView addSubview:_usernameLabel];
+        
+        _dateLabel = [UILabel new];
+        _dateLabel.text = @"2017/11/11";
+        _dateLabel.textColor = COMMON_TEXT_PLACEHOLDER_COLOR;
+        _dateLabel.font = PINGFANG_FONT_OF_SIZE(9);
+        [_dateLabel sizeToFit];
+        _dateLabel.right = SCREEN_WIDTH-ROUND_WIDTH_FLOAT(15);
+        _dateLabel.centerY = _avatarImageView.centerY;
+        [self.contentView addSubview:_dateLabel];
+        
+        _contentLabel = [UILabel new];
+        _contentLabel.text = @"效率文具控的安利《这可能是最全的抵达清单可能是最全的抵达清单（TickTick）使用指南了》";
+        _contentLabel.textColor = COMMON_TEXT_CONTENT_COLOR;
+        _contentLabel.font = PINGFANG_FONT_OF_SIZE(10);
+        _contentLabel.numberOfLines = 2;
+        _contentLabel.size = CGSizeMake(ROUND_WIDTH_FLOAT(250), ROUND_WIDTH_FLOAT(30));
+        _contentLabel.top = _usernameLabel.bottom +10;
+        _contentLabel.left = _usernameLabel.left;
+        [self.contentView addSubview:_contentLabel];
+        
+        UIView *underLine = [[UIView alloc] initWithFrame:CGRectMake(_usernameLabel.left, _contentLabel.bottom+15, SCREEN_WIDTH-_usernameLabel.left-ROUND_WIDTH_FLOAT(15), 0.5)];
+        underLine.backgroundColor = COMMON_SEPARATOR_COLOR;
+        [self.contentView addSubview:underLine];
+        
+        [self layoutIfNeeded];
+        self.cellHeight = underLine.bottom;        
+    }
+    return self;
 }
 
 @end
--- ShirokarasNotebook/UI/Notification/SKNotificationViewController.m
+++ ShirokarasNotebook/UI/Notification/SKNotificationViewController.m
@@ -7,31 +7,177 @@
 //
 
 #import "SKNotificationViewController.h"
+#import "SKNotificationTableViewCell.h"
+#import "SKSegmentView.h"
 
-@interface SKNotificationViewController ()
+#define HEADERVIEW_HEIGHT (64+ROUND_HEIGHT_FLOAT(22))
+#define TITLEVIEW_WIDTH SCREEN_WIDTH
+#define TITLEVIEW_HEIGHT ROUND_HEIGHT_FLOAT(44)
 
+typedef NS_ENUM(NSInteger, SKNotificationSelectedType) {
+    SKMarketSelectedTypeTicket,
+    SKMarketSelectedTypeShop
+};
+
+@interface SKNotificationViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray *dataArray;
+
+@property (nonatomic, strong) SKSegmentView *titleView;
+@property (nonatomic, strong) UIButton *button_follow;
+@property (nonatomic, strong) UIButton *button_hot;
+@property (nonatomic, strong) UIView *markLine;
+@property (nonatomic, assign) SKNotificationSelectedType selectedType;
 @end
 
 @implementation SKNotificationViewController
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.navigationController.navigationBar setHidden:YES];
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent animated:YES];
+    [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationFade];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = COMMON_BG_COLOR;
+    [self createUI];
+    [self addObserver:self forKeyPath:@"selectedType" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:nil];
+}
+
+- (void)dealloc {
+    [self removeObserver:self forKeyPath:@"selectedType"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)createUI {
+    //TableView
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT-49) style:UITableViewStylePlain];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    self.tableView.backgroundColor = COMMON_BG_COLOR;
+    self.tableView.showsVerticalScrollIndicator = NO;
+    [self.tableView registerClass:[SKNotificationTableViewCell class] forCellReuseIdentifier:NSStringFromClass([SKNotificationTableViewCell class])];
+    [self.view addSubview:_tableView];
+    
+    
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, HEADERVIEW_HEIGHT+40)];
+    headerView.backgroundColor = [UIColor clearColor];
+    
+    UIImageView *headerImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, HEADERVIEW_HEIGHT)];
+    headerImageView.backgroundColor = COMMON_BG_COLOR;
+    [headerView addSubview:headerImageView];
+    
+    UIView *blankView = [[UIView alloc] initWithFrame:CGRectMake(0, HEADERVIEW_HEIGHT, SCREEN_WIDTH, ROUND_WIDTH_FLOAT(22))];
+    [headerView addSubview:blankView];
+    self.tableView.tableHeaderView = headerView;
+    
+#ifdef __IPHONE_11_0
+    if ([self.tableView respondsToSelector:@selector(setContentInsetAdjustmentBehavior:)]) {
+        if (@available(iOS 11.0, *)) {
+            self.tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+#endif
+    
+    //TitleView
+    _titleView = [[SKSegmentView alloc] initWithFrame:CGRectMake(0, 0, TITLEVIEW_WIDTH, TITLEVIEW_HEIGHT) titleNameArray:@[@"通知", @"评论", @"赞", @"@我"]];
+    _titleView.layer.cornerRadius = 3;
+    _titleView.backgroundColor = [UIColor whiteColor];
+    _titleView.top = HEADERVIEW_HEIGHT-TITLEVIEW_HEIGHT/2;
+    _titleView.centerX = self.view.centerX;
+    _titleView.userInteractionEnabled = YES;
+    [self.tableView addSubview:_titleView];
 }
-*/
 
+- (void)didClickFollowButton:(UIButton*)sender {
+    self.selectedType = SKMarketSelectedTypeTicket;
+}
+
+- (void)didClickHotButton:(UIButton*)sender {
+    self.selectedType = SKMarketSelectedTypeShop;
+}
+
+#pragma mark - UITableView Delegate
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    SKNotificationTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([SKNotificationTableViewCell class])];
+    if (cell==nil) {
+        cell = [[SKNotificationTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:NSStringFromClass([SKNotificationTableViewCell class])];
+    }
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    SKNotificationTableViewCell *cell = (SKNotificationTableViewCell *)[self tableView:tableView cellForRowAtIndexPath:indexPath];
+    return cell.cellHeight+15;
+}
+
+#pragma mark - UITableView DataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    //    return _dataArray.count;
+    return 5;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+#pragma mark - ScrollView Delegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    DLog(@"%lf", scrollView.contentOffset.y);
+    if (scrollView.contentOffset.y<HEADERVIEW_HEIGHT-TITLEVIEW_HEIGHT/2) {
+        [UIView animateWithDuration:0.2 animations:^{
+            _titleView.left = (self.view.width-TITLEVIEW_WIDTH)/2;
+            _titleView.height = TITLEVIEW_HEIGHT;
+            _titleView.width = TITLEVIEW_WIDTH;
+            _titleView.layer.cornerRadius = 5;
+            
+            for (UIView *view in _titleView.subviews) {
+                if ([view isKindOfClass:[UIButton class]]) {
+                    view.top =0;
+                }
+            }
+            
+            _titleView.markLine.bottom = _titleView.height;
+        } completion:^(BOOL finished) {
+            [_titleView removeFromSuperview];
+            _titleView.frame = CGRectMake((self.view.width-_titleView.width)/2, HEADERVIEW_HEIGHT-TITLEVIEW_HEIGHT/2, TITLEVIEW_WIDTH, TITLEVIEW_HEIGHT);
+            [self.tableView addSubview:_titleView];
+        }];
+    } else {
+        [UIView animateWithDuration:0.2 animations:^{
+            _titleView.left = 0;
+            _titleView.width = SCREEN_WIDTH;
+            _titleView.layer.cornerRadius = 0;
+            
+            for (UIView *view in _titleView.subviews) {
+                if ([view isKindOfClass:[UIButton class]]) {
+                    view.top =20;
+                }
+            }
+            
+            _titleView.markLine.bottom = _titleView.height;
+        } completion:^(BOOL finished) {
+            [_titleView removeFromSuperview];
+            _titleView.frame = CGRectMake(0, 0, SCREEN_WIDTH, 20+TITLEVIEW_HEIGHT);
+            [self.view addSubview:_titleView];
+        }];
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"selectedType"]) {
+        
+    }
+}
 @end
