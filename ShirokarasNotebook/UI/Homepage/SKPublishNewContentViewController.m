ac7e298c741f89655853256cfc6f7d0456830f90
SinLemon <shirokaras@qq.com>
2017-11-29 17:52:08 +0800

    Update - Publish view

--- ShirokarasNotebook/UI/Homepage/SKPublishNewContentViewController.m
+++ ShirokarasNotebook/UI/Homepage/SKPublishNewContentViewController.m
@@ -10,6 +10,7 @@
 
 @interface SKPublishNewContentViewController ()
 @property (nonatomic, strong) UITextView *textView;
+@property (nonatomic, strong) UILabel *textCountLabel;
 @end
 
 @implementation SKPublishNewContentViewController
@@ -21,13 +22,27 @@
     [self createTitleView];
     
     
-    self.textView = [[UITextView alloc] initWithFrame:CGRectMake(20, 100, 300, 300)];
+    self.textView = [[UITextView alloc] initWithFrame:CGRectMake(15, 20+ROUND_WIDTH_FLOAT(44+15), self.view.width-30, ROUND_WIDTH_FLOAT(105))];
     _textView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:_textView];
+    
+    _textCountLabel = [UILabel new];
+    _textCountLabel.text = @"200/200";
+    _textCountLabel.textColor = COMMON_TEXT_PLACEHOLDER_COLOR;
+    _textCountLabel.font = PINGFANG_ROUND_FONT_OF_SIZE(9);
+    [_textCountLabel sizeToFit];
+    _textCountLabel.top = _textView.bottom+ROUND_WIDTH_FLOAT(15);
+    _textCountLabel.right = _textView.right;
+    [self.view addSubview:_textCountLabel];
+    
     [[_textView.rac_textSignal filter:^BOOL(NSString *value) {
         return value;
     }]
      subscribeNext:^(NSString *x) {
+         //更新字数
+         _textCountLabel.text = [NSString stringWithFormat:@"%ld/200", x.length];
+         [_textCountLabel sizeToFit];
+         _textCountLabel.right = _textView.right;
          // 话题的规则
          NSString *topicPattern = @"#[0-9a-zA-Z\\u4e00-\\u9fa5]+#";
          // @的规则
@@ -37,15 +52,23 @@
          NSRegularExpression *regex = [[NSRegularExpression alloc] initWithPattern:pattern options:0 error:nil];
          //匹配集合
          NSArray *results = [regex matchesInString:x options:0 range:NSMakeRange(0, x.length)];
+         
+         NSMutableAttributedString * attrStr = [[NSMutableAttributedString alloc] initWithData:[x dataUsingEncoding:NSUnicodeStringEncoding]
+                                                                                       options:@{NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType}
+                                                                            documentAttributes:nil error:nil];
          // 3.遍历结果
          for (NSTextCheckingResult *result in results) {
              NSLog(@"%@  %@",NSStringFromRange(result.range),[x substringWithRange:result.range]);
-             NSMutableAttributedString * attrStr = [[NSMutableAttributedString alloc] initWithData:[x dataUsingEncoding:NSUnicodeStringEncoding] options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType } documentAttributes:nil error:nil];
+             //set font
+             [attrStr addAttribute:NSFontAttributeName value:PINGFANG_FONT_OF_SIZE(14) range:NSMakeRange(0, x.length)];
              // 设置颜色
              [attrStr addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:result.range];
-             self.textView.attributedText = attrStr;
          }
+         self.textView.attributedText = attrStr;
+         _textView.font = PINGFANG_FONT_OF_SIZE(14);
      }];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
