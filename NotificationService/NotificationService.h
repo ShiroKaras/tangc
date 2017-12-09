edceff02692e1af5c05c17f13c66b98e04d14c0f
SinLemon <shirokaras@qq.com>
2017-12-09 13:28:47 +0800

    Add - Getui

---
+++ NotificationService/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  NotificationService
+//
+//  Created by SinLemon on 2017/12/9.
+//  Copyright © 2017年 SinLemon. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
