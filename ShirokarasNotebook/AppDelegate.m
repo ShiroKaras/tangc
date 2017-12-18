7cc675e40680b5d872226ec843c4152d34a56cf3
SinLemon <shirokaras@qq.com>
2017-12-18 19:24:58 +0800

    Update - weixin login

--- ShirokarasNotebook/AppDelegate.m
+++ ShirokarasNotebook/AppDelegate.m
@@ -256,8 +256,8 @@
                                             authType:SSDKAuthTypeSSO];
                  break;
              case SSDKPlatformTypeWechat:
-                 [appInfo SSDKSetupWeChatByAppId:@"wx823b3364e93fb47a"
-                                       appSecret:@"e21e3553124737a8a505ed5ea07c0526"];
+                 [appInfo SSDKSetupWeChatByAppId:@"wxb584395fb2e50f8c"
+                                       appSecret:@"fa57ec44c1dcc9216fb9b620ee5d1267"];
                  break;
              case SSDKPlatformTypeQQ:
                  [appInfo SSDKSetupQQByAppId:@"1106454935"
@@ -269,62 +269,6 @@
                  break;
          }
      }];
-    
-    
-//    /**
-//     *  设置ShareSDK的appKey，如果尚未在ShareSDK官网注册过App，请移步到http://mob.com/login
-//     * 登录后台进行应用注册，
-//     *  在将生成的AppKey传入到此方法中。
-//     *  方法中的第二个第三个参数为需要连接社交平台SDK时触发，
-//     *  在此事件中写入连接代码。第四个参数则为配置本地社交平台时触发，根据返回的平台类型来配置平台信息。
-//     *  如果您使用的时服务端托管平台信息时，第二、四项参数可以传入nil，第三项参数则根据服务端托管平台来决定要连接的社交SDK。
-//     */
-//
-//    // 117f8a0b99f70
-//    [ShareSDK registerApp:[[ClientConfiguration sharedInstance] ShareSDKAppKey]
-//
-//          activePlatforms:@[
-//                            @(SSDKPlatformTypeSinaWeibo), @(SSDKPlatformTypeWechat), @(SSDKPlatformTypeQQ)
-//                            ]
-//                 onImport:^(SSDKPlatformType platformType) {
-//                     switch (platformType) {
-//                         case SSDKPlatformTypeWechat:
-//                             [ShareSDKConnector connectWeChat:[WXApi class]];
-//                             break;
-//                         case SSDKPlatformTypeQQ:
-//                             [ShareSDKConnector connectQQ:[QQApiInterface class]
-//                                        tencentOAuthClass:[TencentOAuth class]];
-//                             break;
-//                         case SSDKPlatformTypeSinaWeibo:
-//                             [ShareSDKConnector connectWeibo:[WeiboSDK class]];
-//                             break;
-//                         default:
-//                             break;
-//                     }
-//                 }
-//          onConfiguration:^(SSDKPlatformType platformType, NSMutableDictionary *appInfo) {
-//
-//              switch (platformType) {
-//                  case SSDKPlatformTypeSinaWeibo:
-//                      //设置新浪微博应用信息,其中authType设置为使用SSO＋Web形式授权
-//                      [appInfo SSDKSetupSinaWeiboByAppKey:[[ClientConfiguration sharedInstance] SSDKPlatformTypeSinaWeiboAppKey]
-//                                                appSecret:[[ClientConfiguration sharedInstance] SSDKPlatformTypeSinaWeiboAppSecret]
-//                                              redirectUri:[[ClientConfiguration sharedInstance] SSDKPlatformTypeSinaWeiboRedirectUri]
-//                                                 authType:SSDKAuthTypeBoth];
-//                      break;
-//                  case SSDKPlatformTypeWechat:
-//                      [appInfo SSDKSetupWeChatByAppId:[[ClientConfiguration sharedInstance] SSDKPlatformTypeWechatAppId]
-//                                            appSecret:[[ClientConfiguration sharedInstance] SSDKPlatformTypeWechatAppSecret]];
-//                      break;
-//                  case SSDKPlatformTypeQQ:
-//                      [appInfo SSDKSetupQQByAppId:[[ClientConfiguration sharedInstance] SSDKPlatformTypeQQAppId]
-//                                           appKey:[[ClientConfiguration sharedInstance] SSDKPlatformTypeQQAppKey]
-//                                         authType:SSDKAuthTypeBoth];
-//                      break;
-//                  default:
-//                      break;
-//              }
-//          }];
 }
 
 #pragma mark - Qiniu
