03ddbaf225fe61adc02cc4178e35f7bc36949bcd
SinLemon <shirokaras@qq.com>
2017-12-19 19:25:27 +0800

    Update

--- ShirokarasNotebook/UI/Homepage/SKHomepageViewController.m
+++ ShirokarasNotebook/UI/Homepage/SKHomepageViewController.m
@@ -364,7 +364,7 @@ typedef NS_ENUM(NSInteger, SKHomepageSelectedType) {
         CHTCollectionViewWaterfallLayout *layout = [[CHTCollectionViewWaterfallLayout alloc] init];
         
         layout.sectionInset = UIEdgeInsetsMake(SPACE, SPACE, 0, SPACE);
-        layout.headerHeight = HEADERVIEW_HEIGHT+ TITLEVIEW_HEIGHT/2 +20+ROUND_WIDTH_FLOAT(68);
+        layout.headerHeight = ROUND_WIDTH_FLOAT(212);
         layout.footerHeight = 0;
         layout.minimumColumnSpacing = SPACE;
         layout.minimumInteritemSpacing = SPACE;
